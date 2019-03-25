845dd5cb2d509832d518852b2ac4ec17cf3c1b2e
JD <jt7903@gmail.com>
2019-03-24 22:57:39 -0400

    initial

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+var ingredients = [];
+
+function showModal() {
+  $('.recipe-modal').css('display', 'flex');
+}
+
+function hideModal() {
+  $('.recipe-modal').hide();
+}
+
+function addIngredient(event) {
+  event.preventDefault();
+
+  var ingredientValue = $('#add').val();
+
+  ingredients.push(ingredientValue);
+
+  $('.pending-ingredients').html('');
+
+  for (var i = 0; i < ingredients.length; i++) {
+    $('.pending-ingredients').append('<li>' + ingredients[i] + '</li>');
+  }
+}
+
+function saveRecipe(event) {
+  event.preventDefault();
+
+  var nameValue = $('#name').val();
+  var instructionsValue = $('#instructions').val();
+  var ingredientValue = $('#add').val();
+
+  console.log(ingredientValue);
+
+}
+
+
+function init() {
+  $('#open-modal').on('click', showModal);
+  $('.close-modal').on('click', hideModal);
+  $('.add').on('click', addIngredient);
+  $('#save').on('click', saveRecipe);
+}
+
+init();
